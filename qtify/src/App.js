5741db7ac3d693bb7da9ac4e9a7db80fc14d8d00
CoderRDX <rahuldev6258@gmail.com>
2024-08-30 02:52:41 +0530

    new changes

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar/Navbar"
 import Hero from './components/Hero/Hero'
 // import SongCard from './components/SongCard/SongCard';
 import Section from './components/Section';
+import Facts from './components/Footer/Footer';
 // import Carousel from './components/Carousel/Carousel';
 
 
@@ -15,6 +16,8 @@ function App() {
        <Hero/>
        <Section api={"https://qtify-backend-labs.crio.do/albums/top"} albumName={"Top Album"}/>
        <Section api={"https://qtify-backend-labs.crio.do/albums/new"} albumName={"New Album"}/>
+       <Section api={"https://qtify-backend-labs.crio.do/songs"} albumName={"Songs"}/>
+       <Facts/>
        
   
       
---
+++ qtify/src/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+function Facts() {
+  return (
+    <div className={styles.faqs}>
+      <div>
+        <h1 className={styles.Text}>FAQs</h1>
+        <h3 className={styles.Text}>Qtify is free to use</h3>
+        <h3 className={styles.Text}>You can't download and listen to songs offline</h3>
+
+      </div>
+      <div>
+       
+      </div>
+    </div>
+  );
+}
+
+export default Facts;
