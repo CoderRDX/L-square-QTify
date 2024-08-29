2fea52ad2fd159b7c4faa28e7055968846364630
CoderRDX <rahuldev6258@gmail.com>
2024-08-29 12:39:52 +0530

    test commit

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Navbar from "./components/Navbar/Navbar"
 import Hero from './components/Hero/Hero'
-import SongCard from './components/SongCard/SongCard';
+// import SongCard from './components/SongCard/SongCard';
 import Section from './components/Section';
 
 
